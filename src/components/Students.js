6545c68ad4081d895f585a4c3e9cc36aa79edb55
aguilana <naguila.mandich@gmail.com>
2022-12-11 18:54:53 -0500

    students component rendering picture and name. successfully fetching data from the redux store

--- src/components/Students.js
+++ src/components/Students.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAllStudents, selectStudents } from "../features/StudentsSlice";
 
 const Students = () => {
-  return (
-    <div>Students</div>
-  )
-}
+  const dispatch = useDispatch();
 
-export default Students
+  const students = useSelector(selectStudents);
+
+  useEffect(() => {
+    dispatch(fetchAllStudents());
+  }, [dispatch]);
+
+  return (
+    <>
+      <h1>All Students</h1>
+      <ul>
+        {students
+          ? students.map((student) => {
+              return (
+                <li>
+                  <img
+                    src={student.imageUrl}
+                    alt={`This is a picture student ${student.firstName} who currently attends ${student.campus.name}`}
+                    style={{ width: 250, height: 250 }}
+                  />
+                  <div>
+                    <span>
+                      <h3>
+                        {student.firstName} {student.lastName}
+                      </h3>
+                    </span>
+                  </div>
+                </li>
+              );
+            })
+          : "no students to display"}
+      </ul>
+    </>
+  );
+};
+
+export default Students;
