5ffd6541796eff101ed1a5119bf97091d81e7b1f
aguilana <naguila.mandich@gmail.com>
2022-12-12 11:48:12 -0500

    rendering single student with full name, email, image gpa and the campus they attend, if any, or a message saying no campus attended

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchSingleStudent, selectStudent } from '../features/StudentSlice';
+
 
 const Student = () => {
 
     const {studentId} = useParams();
 
+    const dispatch = useDispatch();
+
+    const student = useSelector(selectStudent)
+
+    const { firstName, lastName, email, imageUrl, gpa} = student
+
+    useEffect(()=>{
+        dispatch(fetchSingleStudent(studentId))
+    },[dispatch])
+
 
   return (
     <>
-        <h1>STUDENT NAME with student id: {studentId}</h1>
-        <h2>IMAGE</h2>
-        <h2>ADDRESS</h2>
-        <h2>DESCRIPTION</h2>
+        <h1>{firstName} {lastName}</h1>
+        <img src={imageUrl} alt={`this is an image of the student ${firstName}, ${lastName}`} style={{width: 250, height: 250}}/>
+        <h2>EMAIL: {email}</h2>
+        <h2>GPA: {gpa}</h2>
+        <h5>{student.campus ? `${firstName} ${lastName} currently attends ${student.campus.name}` : `${firstName} is currently not associated with any campus!`}</h5>
     </>
   )
 }
