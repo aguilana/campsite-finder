cb02feee96d3e5bc6d026420c844ec351e5f5cb1
aguilana <naguila.mandich@gmail.com>
2022-12-12 12:07:01 -0500

    added campuses link on each student that attends a campus

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,33 +1,44 @@
-import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchSingleStudent, selectStudent } from '../features/StudentSlice';
-
+import React, { useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchSingleStudent, selectStudent } from "../features/StudentSlice";
 
 const Student = () => {
+  const { studentId } = useParams();
 
-    const {studentId} = useParams();
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const student = useSelector(selectStudent);
 
-    const student = useSelector(selectStudent)
+  const { firstName, lastName, email, imageUrl, gpa, campus } = student;
 
-    const { firstName, lastName, email, imageUrl, gpa} = student
-
-    useEffect(()=>{
-        dispatch(fetchSingleStudent(studentId))
-    },[dispatch])
-
+  useEffect(() => {
+    dispatch(fetchSingleStudent(studentId));
+  }, [dispatch]);
 
   return (
     <>
-        <h1>{firstName} {lastName}</h1>
-        <img src={imageUrl} alt={`this is an image of the student ${firstName}, ${lastName}`} style={{width: 250, height: 250}}/>
-        <h2>EMAIL: {email}</h2>
-        <h2>GPA: {gpa}</h2>
-        <h5>{student.campus ? `${firstName} ${lastName} currently attends ${student.campus.name}` : `${firstName} is currently not associated with any campus!`}</h5>
+      <h1>
+        {firstName} {lastName}
+      </h1>
+      <img
+        src={imageUrl}
+        alt={`this is an image of the student ${firstName}, ${lastName}`}
+        style={{ width: 250, height: 250 }}
+      />
+      <h2>EMAIL: {email}</h2>
+      <h2>GPA: {gpa}</h2>
+      <h5>
+        {student.campus ? (
+          <Link to={`/campuses/${campus.id}`}>
+            {firstName} {lastName} attends {student.campus.name}
+          </Link>
+        ) : (
+          `${firstName} is currently not associated with any campus!`
+        )}
+      </h5>
     </>
-  )
-}
+  );
+};
 
-export default Student
+export default Student;
