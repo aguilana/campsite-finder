de585f376927b3866ddc64f8896fc6a9b866efa0
aguilana <naguila.mandich@gmail.com>
2022-12-12 12:46:47 -0500

    finished the navigating to campus view. link added. moving onto linking the students

--- src/components/Campus.js
+++ src/components/Campus.js
@@ -1,29 +1,42 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { fetchSingleCampus, selectCampus } from '../features/CampusSlice';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { fetchSingleCampus, selectCampus } from "../features/CampusSlice";
 
 const Campus = () => {
+  const { campusId } = useParams();
+  const dispatch = useDispatch();
 
-    const { campusId } = useParams()
-    const dispatch = useDispatch();
+  const campus = useSelector(selectCampus);
+  console.log("this is the campus selected", campus);
+  const { name, imageUrl, address, description, students } = campus;
 
-    const campus = useSelector(selectCampus)
-    console.log("this is the campus selected", campus)
-    const { name, imageUrl, address, description, students } = campus
-
-    useEffect(()=>{
-        dispatch(fetchSingleCampus(campusId))
-    }, [dispatch])
+  useEffect(() => {
+    dispatch(fetchSingleCampus(campusId));
+  }, [dispatch]);
 
   return (
     <div>
-        <h1>{name}</h1>
-        <img src={imageUrl} alt={name} />
-        <h4>{address}</h4>
-        <p>{description}</p>
+      <h1>{name}</h1>
+      <img src={imageUrl} alt={name} />
+      <h4>{address}</h4>
+      <p>{description}</p>
+      <div>
+        <h3>List of attending students</h3>
+        <ul>
+          {students && students.length
+            ? students.map((student) => {
+                return (
+                  <li>
+                    {student.firstName} {student.lastName}
+                  </li>
+                );
+              })
+            : "Campus does not have any students :("}
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Campus
+export default Campus;
--- src/components/Campuses.js
+++ src/components/Campuses.js
@@ -1,39 +1,43 @@
-import React from "react";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAllCampuses, selectCampuses } from "../features/CampusesSlice";
+import { Link } from "react-router-dom";
 
 const Campuses = () => {
   const dispatch = useDispatch();
   const campuses = useSelector(selectCampuses);
   console.log(campuses);
 
-
   useEffect(() => {
     dispatch(fetchAllCampuses());
   }, [dispatch]);
 
   return (
-    <div>
+    <>
       <h1>CAMPUS COMPONENT</h1>
       <ul>
         {campuses
           ? campuses.map((campus) => {
               return (
-                <li key={campus.id}>
-                    <img src={campus.imageUrl} alt={campus.name} style={{width: 250, height: 250}}/>
-                    <div>
-                    <h3>{campus.name}</h3>
+                <li>
+                  <img
+                    src={campus.imageUrl}
+                    alt={campus.name}
+                    style={{ width: 250, height: 250 }}
+                  />
+                  <div>
+                    <Link to={`/campuses/${campus.id}`} key={campus.id}>
+                      <h3>{campus.name}</h3>
+                    </Link>
                     <h4>{campus.address}</h4>
                     <p>{campus.description}</p>
-                    </div>
-
+                  </div>
                 </li>
-              )
+              );
             })
           : "Oops...no campuses listed"}
       </ul>
-    </div>
+    </>
   );
 };
 
