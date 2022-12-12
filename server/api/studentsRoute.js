22ed152769f692c0ffb1f3fdc193fed7785ccdbb
aguilana <naguila.mandich@gmail.com>
2022-12-12 08:27:59 -0500

    single student route is up and running

--- server/api/studentsRoute.js
+++ server/api/studentsRoute.js
@@ -6,12 +6,32 @@ router.get("/", async (req, res, next) => {
   try {
     const students = await Student.findAll({
       attributes: ["id", "firstName", "lastName", "email", "imageUrl", "gpa"],
-      include: [{
-        model: Campus,
-        attributes: ['id', 'name', 'imageUrl', 'address', 'description']
-        }],
+      include: [
+        {
+          model: Campus,
+          attributes: ["id", "name", "imageUrl", "address", "description"],
+        },
+      ],
     });
     res.send(students);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// GET /api/students/:id
+router.get("/:id", async (req, res, next) => {
+  try {
+    const student = await Student.findByPk(req.params.id, {
+      attributes: ["id", "firstName", "lastName", "email", "imageUrl", "gpa"],
+      include: [
+        {
+          model: Campus,
+          attributes: ["id", "name", "imageUrl", "address", "description"],
+        },
+      ],
+    });
+    res.send(student);
   } catch (err) {
     next(err);
   }
