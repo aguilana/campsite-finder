a2024bfe259533805388707ba7b40e393b8ec206
aguilana <naguila.mandich@gmail.com>
2023-02-22 18:35:08 -0500

    gitignore updated and added more dependencies - jwt, bcrypt, styling - and fixed error in create new campground

--- src/components/features/CreateCampground.js
+++ src/components/features/CreateCampground.js
@@ -1,29 +1,29 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { createCampgroundAsync } from "../../features/campgroundSlice";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { createCampgroundAsync } from '../../features/campgroundSlice';
+import { Link } from 'react-router-dom';
 
 const CreateCampground = () => {
-  const [name, setName] = useState("");
-  const [price, setPrice] = useState("");
-  const [description, setDescription] = useState("");
-  const [location, setLocation] = useState("");
-  const [imageUrl, setImage] = useState("");
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [description, setDescription] = useState('');
+  const [location, setLocation] = useState('');
+  const [imageUrl, setImage] = useState('');
 
   const dispatch = useDispatch();
 
   const handleChange = (e) => {
-    if (e.target.name === "name") setName(e.target.value);
-    if (e.target.name === "price") setPrice(e.target.value);
-    if (e.target.name === "description") setDescription(e.target.value);
-    if (e.target.name === "location") setLocation(e.target.value);
-    if (e.target.name === "imageUrl") setImage(e.target.value);
+    if (e.target.name === 'name') setName(e.target.value);
+    if (e.target.name === 'price') setPrice(e.target.value);
+    if (e.target.name === 'description') setDescription(e.target.value);
+    if (e.target.name === 'location') setLocation(e.target.value);
+    if (e.target.name === 'imageUrl') setImage(e.target.value);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      if (name == "" || price == "" || description == "" || location == "") {
+      if (name == '' || price == '' || description == '' || location == '') {
         alert("can't create campground. Please fill out required fields");
       } else {
         await dispatch(
@@ -35,17 +35,17 @@ const CreateCampground = () => {
             imageUrl,
           })
         );
-        setName("");
-        setPrice("");
-        setDescription("");
-        setLocation("");
+        setName('');
+        setPrice('');
+        setDescription('');
+        setLocation('');
         alert(
-          "new campground created. We will redirect you to that campground"
+          'new campground created. We will redirect you to that campground'
         );
         // navigate to the /campgrounds/:id
       }
     } catch (err) {
-      throw new Error("Error: ", err);
+      throw new Error('Error: ', err);
     }
   };
 
@@ -53,65 +53,63 @@ const CreateCampground = () => {
     <>
       <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="name">Name: </label>
+          <label htmlFor='name'>Name: </label>
           <input
-            type="text"
-            name="name"
+            type='text'
+            name='name'
             value={name}
             onChange={handleChange}
             required
           />
         </div>
         <div>
-          <label htmlFor="price">Price: $</label>
+          <label htmlFor='price'>Price: $</label>
           <input
-            type="text"
-            name="price"
+            type='text'
+            name='price'
             value={price}
             onChange={handleChange}
             required
           />
         </div>
         <div>
-          <label htmlFor="description">Description: </label>
+          <label htmlFor='description'>Description: </label>
 
           <textarea
-            type="text"
-            name="description"
+            type='text'
+            name='description'
             value={description}
             onChange={handleChange}
             required
-            cols="50"
-            rows="10"
-            style={{ maxHeight: "100px", maxWidth: "350px" }}
-            placeholder="description"
+            cols='50'
+            rows='10'
+            style={{ maxHeight: '100px', maxWidth: '350px' }}
+            placeholder='description'
           />
         </div>
         <div>
-          <label htmlFor="location">Location: </label>
+          <label htmlFor='location'>Location: </label>
           <input
-            type="text"
-            name="location"
+            type='text'
+            name='location'
             value={location}
             onChange={handleChange}
             required
           />
         </div>
         <div>
-          <label htmlFor="imageUrl">Image Url: </label>
+          <label htmlFor='imageUrl'>Image Url: </label>
           <input
-            type="text"
-            name="imageUrl"
+            type='text'
+            name='imageUrl'
             value={imageUrl}
             onChange={handleChange}
           />
         </div>
-        <button type="submit" onClick={handleClick}>
-          Create Campground
-        </button>
+        <button type='submit'>Create Campground</button>
       </form>
       <footer>
-        <Link to="/campgrounds">All Campgrounds</Link>
+        <Link to='/campgrounds'>All Campgrounds</Link>
       </footer>
     </>
   );
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,37 @@
+const Dotenv = require('dotenv-webpack');
 module.exports = {
-  mode: "development",
-  entry: ["./src/index.js"],
+  mode: 'development',
+  entry: ['./src/index.js'],
   output: {
-    path: __dirname + "/public",
-    filename: "bundle.js",
+    path: __dirname + '/public',
+    filename: 'bundle.js',
   },
   context: __dirname,
-  devtool: "source-map",
+  devtool: 'source-map',
   devServer: {
     static: {
-      directory: __dirname + "/public",
+      directory: __dirname + '/public',
     },
   },
+  // this allows .env to happen
+  plugins: [
+    new Dotenv({
+      path: '.env', // or '.env.local', '.env.[mode]', etc.
+    }),
+  ],
   module: {
     rules: [
       {
+        // this allows css styling loader
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
         test: /jsx?$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
+          presets: ['@babel/preset-env', '@babel/preset-react'],
         },
       },
     ],
